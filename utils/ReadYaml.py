f03b0b093e4832191dcecbef39aaa81a241dffed
zexin.xu <zexinnn.126.com>
2021-08-27 17:29:14 +0800

    version 1.1

--- utils/ReadYaml.py
+++ utils/ReadYaml.py
@@ -14,5 +14,5 @@ def ReadYaml(yaml_dic_name):
     current_path = os.path.dirname(os.path.realpath(__file__))
     yaml_path = os.path.join(os.path.abspath(os.path.join(current_path, "..")), yaml_dic_name)
     f = open(yaml_path, 'r', encoding='utf-8',  errors='ignore')
-    data = yaml.load(f.read())
+    data = yaml.load(f.read(), Loader=yaml.BaseLoader)
     return data
